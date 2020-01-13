507daa47d231730718711508deb247ff2bfa5228
pavelY <yurtaev.ps@gmail.com>
2020-01-13 22:54:12 +0300

    simple sorting

--- src/main/java/edu/algorithms/arrays/SimpleArray.java
+++ src/main/java/edu/algorithms/arrays/SimpleArray.java
@@ -33,11 +33,55 @@ class SimpleArray {
                 maxIndex = i;
             }
         }
-        for (int i = maxIndex; i < array.length - 1 ; i++) {
+        for (int i = maxIndex; i < array.length - 1; i++) {
             array[i] = array[i + 1];
         }
         int[] result = new int[array.length - 1];
         System.arraycopy(array, 0, result, 0, array.length - 1);
         return result;
+    }
+
+    public int[] bubbleSort() {
+        for (int out = array.length - 1; out > 1; out--) {
+            for (int in = 0; in < out; in++) {
+                if (array[in] > array[in + 1]) {
+                    int temp = array[in];
+                    array[in] = array[in + 1];
+                    array[in + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public int[] selectionSort() {
+        int min;
+        for (int out = 0; out < array.length - 1; out++) {
+            min = out;
+            for (int in = out + 1; in < array.length; in++) {
+                if (array[in] < array[min]) {
+                    min = in;
+                }
+            }
+            int temp = array[out];
+            array[out] = array[min];
+            array[min] = temp;
+        }
+        return array;
+    }
+
+    public int[] insertionSort() {
+        int in;
+        for(int out = 1; out < array.length; out++) {
+            int temp = array[out];
+            // start shifts at out
+            in = out;
+            while(in > 0 && array[in - 1] >= temp) {
+                array[in] = array[in - 1];
+                --in;
+            }
+            array[in] = temp;
+        }
+        return array;
     }
 }
--- src/test/java/edu/algorithms/arrays/SimpleArrayTest.java
+++ src/test/java/edu/algorithms/arrays/SimpleArrayTest.java
@@ -23,4 +23,25 @@ public class SimpleArrayTest {
         SimpleArray simpleArray = new SimpleArray(testArr);
         assertArrayEquals(new int[]{2, 5, 14, 8, 3, 7, 13, 9}, simpleArray.removeMax());
     }
+
+    @Test
+    public void bubbleSortTest() {
+        int[] testArr = getTestArray();
+        SimpleArray simpleArray = new SimpleArray(testArr);
+        assertArrayEquals(new int[]{2, 3, 5, 7, 8, 9, 13, 14, 18}, simpleArray.bubbleSort() );
+    }
+
+    @Test
+    public void selectionSortTest() {
+        int[] testArr = getTestArray();
+        SimpleArray simpleArray = new SimpleArray(testArr);
+        assertArrayEquals(new int[]{2, 3, 5, 7, 8, 9, 13, 14, 18}, simpleArray.selectionSort());
+    }
+
+    @Test
+    public void insertionSortTest() {
+        int[] testArr = getTestArray();
+        SimpleArray simpleArray = new SimpleArray(testArr);
+        assertArrayEquals(new int[]{2, 3, 5, 7, 8, 9, 13, 14, 18}, simpleArray.insertionSort());
+    }
 }
